62c17c9b7c00617117ff3166f34f755a1ff3ab53
nhudo <nhudo267@gmail.com>
2019-02-16 23:08:25 +0700

    m

---
+++ js-practices/handout/Practice1/searchPractice.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// function search(input, target) {
+//   for (const target of input) {
+//     console.log(target);
+
+//   }
+//   return  input.indexOf(target);  // Remove this line and change to your own algorithm
+// }
+
+// module.exports = search
+
+
+function search(input, target) {
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] == target) {
+            return i;
+        } else {
+            return -1;
+        }
+    }
+}
