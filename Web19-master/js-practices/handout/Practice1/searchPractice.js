d24228c4b6999e9178ebde24653ab61befe90396
nhudo <nhudo267@gmail.com>
2019-02-17 19:36:01 +0700

    17/2

---
+++ Web19-master/js-practices/handout/Practice1/searchPractice.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function search(input, target) {
+    return input.indexOf(target); // Remove this line and change to your own algorithm
+}
+
+module.exports = search
+
+
+// function search(input, target) {
+//     for (let i = 0; i < input.length; i++) {
+//         if (input[i] == target) {
+//             return i;
+//         } else {
+//             return -1;
+//         }
+//     }
+// }
